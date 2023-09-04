98b4008237610966bbb06d93748503a92176d7fa
Taha Mohamed <tahamohamedman@gmail.com>
2023-09-04 11:37:55 +0200

    Adding top nav

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SideBarComponent } from './side-bar/side-bar.component';
+import { TopNavComponent } from './top-nav/top-nav.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SideBarComponent
+    SideBarComponent,
+    TopNavComponent
   ],
   imports: [
     BrowserModule,
