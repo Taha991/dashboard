220416fab3ad44a1084efd5c37a2238ea8938452
Taha Mohamed <tahamohamedman@gmail.com>
2023-09-05 12:52:41 +0200

    Fixing chart bug

--- src/app/dashboard-body/dashboard-body.component.ts
+++ src/app/dashboard-body/dashboard-body.component.ts
@@ -1,4 +1,4 @@
-import { Component, ViewChild, AfterViewInit } from '@angular/core';
+import { Component, ViewChild, AfterViewInit, ElementRef } from '@angular/core';
 import { Chart, registerables } from 'chart.js';
 
 @Component({
@@ -6,63 +6,77 @@ import { Chart, registerables } from 'chart.js';
   templateUrl: './dashboard-body.component.html',
   styleUrls: ['./dashboard-body.component.css']
 })
-export class DashboardBodyComponent implements AfterViewInit  {
-   // Track the current view (initially set to 'chart')
-   repeatCount = 2; // Specify the number of times to repeat the component
-
-   currentView = 'chart';
-  @ViewChild('mychart') mychart: any;
-  @ViewChild('radarChart') radarChart: any;
+export class DashboardBodyComponent implements AfterViewInit {
+  repeatCount = 2;
+  currentView = 'chart';
+  @ViewChild('mychart', { static: false }) mychart!: ElementRef<HTMLCanvasElement>;
+  @ViewChild('radarChart', { static: false }) radarChart!: ElementRef<HTMLCanvasElement>;
   chart!: Chart;
+  radarChartInstance!: Chart;
 
-   // Function to switch views
-   switchView(view: string) {
+  constructor() {
+    Chart.register(...registerables);
+  }
+
+  ngAfterViewInit(): void {
+    this.createChart();
+    this.createRadarChart();
+  }
+
+  switchView(view: string) {
     this.currentView = view;
+
+    // If switching back to the chart view, recreate the chart
+    if (view === 'chart') {
+      this.createChart();
+    }
   }
 
   repeatArray(count: number): any[] {
     return new Array(count);
   }
-  ngAfterViewInit(): void {
-    this.createChart();
-  }
 
   createChart(): void {
-    Chart.register(...registerables);
+    if (this.chart) {
+      this.chart.destroy(); // Destroy the existing chart instance
+    }
 
     this.chart = new Chart(this.mychart.nativeElement, {
-      type: 'bar', // Choose the chart type you need
+      type: 'bar',
       data: {
-        labels: ['Station Dummy 1', 'Station Dummy 1', 'Station Dummy 1' , 'Station Dummy 1' ,'Station Dummy 1'],
+        labels: ['Station Dummy 1', 'Station Dummy 1', 'Station Dummy 1', 'Station Dummy 1', 'Station Dummy 1'],
         datasets: [
           {
             label: 'STATIONS ACTIVATES OVERVIEW',
-            data: [120, 90, 150 , 100 , 200 , 300],
-            backgroundColor: '#FF7F5C)',
+            data: [120, 90, 150, 100, 200, 300],
+            backgroundColor: '#FF7F5C',
             borderColor: '#FF7F5C',
             borderWidth: 1,
             barThickness: 11,
-
           },
-
         ],
       },
       options: {}
     });
+  }
 
-    this.chart = new Chart(this.radarChart.nativeElement, {
+  createRadarChart(): void {
+    if (this.radarChartInstance) {
+      this.radarChartInstance.destroy(); // Destroy the existing radar chart instance
+    }
+
+    this.radarChartInstance = new Chart(this.radarChart.nativeElement, {
       type: 'radar',
       data: {
-        labels: ['24:00', '02:00', '04:00', '06:00', '08:00' , '10:00' , '12:00' , '14:00', '16:00' , '18:00' ,
-      '20:00' , '22:00'],
+        labels: ['24:00', '02:00', '04:00', '06:00', '08:00', '10:00', '12:00', '14:00', '16:00', '18:00', '20:00', '22:00'],
         datasets: [
           {
             label: 'Data',
-            data: [120, 90,300, 100, 200 , 300 , 400],
+            data: [120, 90, 300, 100, 200, 300, 400],
             backgroundColor: '#009ACF',
             borderColor: '#009ACF33',
             borderWidth: 1,
-            pointStyle: 'circle', // You can use 'circle' to make it look like a radar point
+            pointStyle: 'circle',
             pointRadius: 0,
           },
         ],
@@ -73,7 +87,7 @@ export class DashboardBodyComponent implements AfterViewInit  {
             display: false,
           },
           tooltip: {
-            enabled: false, // Disable tooltips
+            enabled: false,
           },
         },
       },
